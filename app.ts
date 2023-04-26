3a39643d0cdf00ce64fec9283a4e668e0ee4e4d4
audhgisadjadnjs <cl10489@g.unicamp.br>
2023-04-26 11:32:45 -0300

    commit aula 04/26

--- app.ts
+++ app.ts
@@ -1,14 +1,46 @@
 import path from "path";
 import express, { Request, Response, request } from 'express';
+const bcrypt = require('bcrypt');
 const app = express();
 const port = 3000;
+
+const numSalt = 12;
+
+
+class Usuario {
+    nome: string;
+    datanasc: string;
+    email: string;
+    celular: string;
+    deficiencia: [];
+    senha: string;
+
+    constructor(nome: string, datanasc: string, email: string, celular: string, deficiencia: [], senha: string)
+    {
+        this.nome = nome;
+        this.datanasc = datanasc;
+        this.email = email;
+        this.celular = celular;
+        this.deficiencia = deficiencia;
+        this.senha = bcrypt.hashSync(senha, numSalt);
+
+        public cadastrar(nome: string, datanasc: string, email: string, celular: string, deficiencia: [], senha: string): boolean
+        {
+            //TODO: Terminar o código para cadastrar o usuário
+            console.log("Cadastrado com sucesso");
+            return false;
+        }
+
+    }
+
+}
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'AccessCityWeb'));
 app.use(express.static(path.join(__dirname, 'AccessCityWeb')));
 
 
-app.get('/', (req: Request, res: Response) =>{
+app.get('/', (req: Request, res: Response) => {
     res.render("index.ejs");
 });
 
