5071c405c4e0ff5a52e318c915fb3f307ae2068d
audhgisadjadnjs <cl10489@g.unicamp.br>
2023-04-26 11:33:01 -0300

    commit aula 04/26

--- app.js
+++ app.js
@@ -5,8 +5,25 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const path_1 = __importDefault(require("path"));
 const express_1 = __importDefault(require("express"));
+const bcrypt = require('bcrypt');
 const app = (0, express_1.default)();
 const port = 3000;
+const numSalt = 12;
+class Usuario {
+    constructor(nome, datanasc, email, celular, deficiencia, senha) {
+        this.nome = nome;
+        this.datanasc = datanasc;
+        this.email = email;
+        this.celular = celular;
+        this.deficiencia = deficiencia;
+        this.senha = bcrypt.hashSync(senha, numSalt);
+    }
+    cadastrar(nome, datanasc, email, celular, deficiencia, senha) {
+        //TODO: Terminar o código para cadastrar o usuário
+        console.log("Cadastrado com sucesso");
+        return false;
+    }
+}
 app.set('view engine', 'ejs');
 app.set('views', path_1.default.join(__dirname, 'AccessCityWeb'));
 app.use(express_1.default.static(path_1.default.join(__dirname, 'AccessCityWeb')));
