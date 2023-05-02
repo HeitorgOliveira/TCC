9dcb892a8a9e39f440fb283e5be92445bbeedad9
reidascapivaras <heitor_portugal1@hotmail.com>
2023-05-02 06:50:25 -0300

    .

--- app.js
+++ app.js
@@ -1,14 +1,41 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const path_1 = __importDefault(require("path"));
 const express_1 = __importDefault(require("express"));
+const body_parser_1 = __importDefault(require("body-parser"));
 const bcrypt = require('bcrypt');
+const mysql = require('mysql');
+const session = require('cookie-session');
 const app = (0, express_1.default)();
 const port = 3000;
 const numSalt = 12;
+const pool = mysql.createPool({
+    host: "143.106.241.3",
+    user: "cl201174",
+    password: "essaehumasenha!",
+    database: "cl201174",
+});
+app.use(session({
+    name: "session",
+    keys: ['key1', 'key2'],
+    cookie: {
+        secure: true,
+        httpOnly: true,
+        path: "/",
+    }
+}));
 class Usuario {
     constructor(nome, datanasc, email, celular, deficiencia, senha) {
         this.nome = nome;
@@ -18,22 +45,54 @@ class Usuario {
         this.deficiencia = deficiencia;
         this.senha = bcrypt.hashSync(senha, numSalt);
     }
-    cadastrar(nome, datanasc, email, celular, deficiencia, senha) {
-        //TODO: Terminar o código para cadastrar o usuário
-        console.log("Cadastrado com sucesso");
-        return false;
+    cadastrar() {
+        return __awaiter(this, void 0, void 0, function* () {
+            try {
+                const connection = yield pool.getConnection();
+                console.log(connection);
+                const resultado = yield connection.execute("INSERT INTO AC_Usuario (nome, datanasc, email, celular, deficiencia, senha) VALUES (?, ?, ?, ?, ?, ?)", [this.nome, this.datanasc, this.email, this.celular, this.deficiencia, this.senha]);
+                connection.release();
+                if (resultado.affectedRows === 1) {
+                    console.log(`AEEEE CADASTREI O ${this.nome}`);
+                    return true;
+                }
+                return false;
+            }
+            catch (err) {
+                console.error(`ERRO: ${err}`);
+                return false;
+            }
+        });
     }
 }
 app.set('view engine', 'ejs');
 app.set('views', path_1.default.join(__dirname, 'AccessCityWeb'));
 app.use(express_1.default.static(path_1.default.join(__dirname, 'AccessCityWeb')));
+app.use(body_parser_1.default.urlencoded({ extended: true }));
+app.use(body_parser_1.default.json());
 app.get('/', (req, res) => {
     res.render("index.ejs");
 });
-app.post('/cadastro', (req, res) => {
+app.post('/cadastro', (req, res) => __awaiter(void 0, void 0, void 0, function* () {
     console.log("Got post request");
-    res.render("index.ejs");
-});
+    const user = req.body.user;
+    const date = req.body.date;
+    const email = req.body.email;
+    const tel = req.body.tel;
+    const deficiencia = req.body.deficiencia;
+    const password = req.body.password;
+    let usuario = new Usuario(user, date, email, tel, deficiencia, password);
+    let resultado = yield usuario.cadastrar();
+    if (resultado) {
+        res.cookie('cadastro', true);
+        res.render('index.ejs');
+    }
+    else {
+        res.cookie('cadastro', false);
+        res.render('index.ejs');
+    }
+    console.log(usuario);
+}));
 app.listen(port, () => {
     console.log(`Aqui: http://localhost:${port}`);
 });
--- app.ts
+++ app.ts
@@ -1,11 +1,30 @@
 import path from "path";
 import express, { Request, Response, request } from 'express';
+import bodyParser from "body-parser";
 const bcrypt = require('bcrypt');
+const mysql = require('mysql');
+const session = require('cookie-session');
 const app = express();
 const port = 3000;
 
 const numSalt = 12;
 
+const pool = mysql.createPool({
+    host: "143.106.241.3",
+    user: "cl201174",
+    password: "essaehumasenha!",
+    database: "cl201174",
+});
+
+app.use(session({
+    name: "session",
+    keys: ['key1', 'key2'],
+    cookie: {
+        secure: true,
+        httpOnly: true,
+        path: "/",
+    }
+}));
 
 class Usuario {
     nome: string;
@@ -23,30 +42,63 @@ class Usuario {
         this.celular = celular;
         this.deficiencia = deficiencia;
         this.senha = bcrypt.hashSync(senha, numSalt);
-
-        public cadastrar(nome: string, datanasc: string, email: string, celular: string, deficiencia: [], senha: string): boolean
-        {
-            //TODO: Terminar o código para cadastrar o usuário
-            console.log("Cadastrado com sucesso");
-            return false;
-        }
-
     }
 
+    async cadastrar(): Promise<boolean>{
+        try {
+          const connection = await pool.getConnection();
+          console.log(connection);
+          const resultado = await connection.execute(
+            "INSERT INTO AC_Usuario (nome, datanasc, email, celular, deficiencia, senha) VALUES (?, ?, ?, ?, ?, ?)",
+            [this.nome, this.datanasc, this.email, this.celular, this.deficiencia, this.senha]
+          );
+          connection.release();
+          if (resultado.affectedRows === 1) {
+            console.log(`AEEEE CADASTREI O ${this.nome}`);
+            return true;
+          }
+          return false;
+        } catch (err) {
+          console.error(`ERRO: ${err}`);
+          return false;
+        }
+      }
+      
+      
 }
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'AccessCityWeb'));
+
 app.use(express.static(path.join(__dirname, 'AccessCityWeb')));
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
 
 
 app.get('/', (req: Request, res: Response) => {
     res.render("index.ejs");
 });
 
-app.post('/cadastro', (req: Request, res: Response) =>{
+app.post('/cadastro', async (req: Request, res: Response) =>{
     console.log("Got post request");
-    res.render("index.ejs");
+    const user = req.body.user;
+    const date = req.body.date;
+    const email = req.body.email;
+    const tel = req.body.tel;
+    const deficiencia = req.body.deficiencia;
+    const password = req.body.password;
+    let usuario = new Usuario(user, date, email, tel, deficiencia, password);
+    let resultado = await usuario.cadastrar();
+
+    if(resultado){
+        res.cookie('cadastro', true);
+        res.render('index.ejs')
+    }
+    else{
+        res.cookie('cadastro', false);
+        res.render('index.ejs')
+    }
+    console.log(usuario);
 })
 
 app.listen(port, ()=>{
